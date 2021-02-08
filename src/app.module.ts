f03cd60bdfd1e71c8e8ab62603618971a39d374e
SaiKrishna1545 <s538340@nwmissouri.edu>
2021-02-08 13:33:08 -0600

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
-import { UserModule } from './user/user.module';
-import { LocationModule } from './location/location.module';
+//import { UserModule } from './user/user.module';
+//import { LocationModule } from './location/location.module';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { Connection } from 'typeorm';
 
